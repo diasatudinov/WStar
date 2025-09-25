c418b520bd8812f43d094a600a378c36ee5fa4e9
Dias <dias.atudinov@gmail.com>
2025-09-26 00:11:21 +0500

    add settings view

--- WStar/WStar/WSModules/WSSettings/CPSettingsViewModel.swift
+++ WStar/WStar/WSModules/WSSettings/CPSettingsViewModel.swift
@@ -1,7 +1,15 @@
+//
+//  CPSettingsViewModel.swift
+//  WStar
+//
+//  Created by Dias Atudinov on 25.09.2025.
+//
+
+
 import SwiftUI
 
 class CPSettingsViewModel: ObservableObject {
+    @AppStorage("musicEnabled") var musicEnabled: Bool = true
     @AppStorage("soundEnabled") var soundEnabled: Bool = true
-    @AppStorage("effectsEnabled") var effectsEnabled: Bool = true
-
+    @AppStorage("vibroEnabled") var vibroEnabled: Bool = true
 }
--- WStar/WStar/WSModules/WSSettings/WSSettingsView.swift
+++ WStar/WStar/WSModules/WSSettings/WSSettingsView.swift
@@ -8,8 +8,86 @@
 import SwiftUI
 
 struct WSSettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject var settingsVM = CPSettingsViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            VStack {
+                VStack(spacing: 70) {
+                    HStack {
+                        Image(.musicTextWS)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 80:35)
+                        Spacer()
+                        Toggle("", isOn: $settingsVM.musicEnabled)
+                            .labelsHidden()
+                            .toggleStyle(SwitchToggleStyle(tint: .toggleBg))
+                    }
+                    
+                    HStack {
+                        Image(.soundsTextWS)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 80:35)
+                        Spacer()
+                        Toggle("", isOn: $settingsVM.soundEnabled)
+                            .labelsHidden()
+                            .toggleStyle(SwitchToggleStyle(tint: .toggleBg))
+                    }
+                    
+                    HStack {
+                        Image(.vibroTextWS)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 80:35)
+                        Spacer()
+                        Toggle("", isOn: $settingsVM.vibroEnabled)
+                            .labelsHidden()
+                            .toggleStyle(SwitchToggleStyle(tint: .toggleBg))
+                    }
+                    
+                }.padding(.horizontal, 50)
+                
+                
+            }
+            
+            VStack {
+                HStack {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                        
+                    } label: {
+                        Image(.backIconWS)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 100:60)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(.settingsIconWS)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 100:75)
+                    
+                    Spacer()
+                    
+                    Image(.backIconWS)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 100:60)
+                        .opacity(0)
+                }.padding()
+                Spacer()
+                
+            }
+        }.frame(maxWidth: .infinity)
+            .background(
+                LinearGradient(colors: [.appBgTop, .appBgBottom], startPoint: .topTrailing, endPoint: .bottomLeading)
+                    .ignoresSafeArea()
+            )
     }
 }
 
