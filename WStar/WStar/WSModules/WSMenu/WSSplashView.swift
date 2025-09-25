56b75db0f8f5543a800db625e44a4ffca46319c4
Dias <dias.atudinov@gmail.com>
2025-09-26 00:11:12 +0500

    add loading view

--- WStar/WStar/WSModules/WSMenu/WSSplashView.swift
+++ WStar/WStar/WSModules/WSMenu/WSSplashView.swift
@@ -2,14 +2,80 @@
 //  WSSplashView.swift
 //  WStar
 //
-//  Created by Dias Atudinov on 25.09.2025.
 //
 
 import SwiftUI
 
 struct WSSplashView: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ZStack {
+                Image(.splashLogoWS)
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+                Color.black.opacity(0.5)
+                    .ignoresSafeArea()
+            }
+            
+            
+            VStack(spacing: 0) {
+                
+                Image(.splashLogoWS)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .cornerRadius(16)
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+                
+                VStack(spacing: 12) {
+                    Text("\(Int(progress * 100))%")
+                        .font(.system(size: 28, weight: .bold, design: .rounded))
+                        .foregroundStyle(.white)
+                        .monospacedDigit()
+                    
+                    ProgressView(value: progress)
+                        .progressViewStyle(.linear)
+                        .frame(width: UIScreen.main.bounds.width/2, height: 8)
+                        .tint(.blue)
+                        .padding(5)
+                        .background(.black.opacity(0.5))
+                        .clipShape(Capsule())
+                }
+                .padding()
+                .onAppear { startTimer() }
+                .onDisappear { timer?.invalidate() }
+            }
+            
+            
+            
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 1 {
+                progress += 0.01
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
