8f253f1d4244bcf847187601a9ee3d4b77f01f7c
Dias <dias.atudinov@gmail.com>
2025-09-25 23:19:41 +0500

    app setup

---
+++ WStar/WStar/WStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WStarApp.swift
+//  WStar
+//
+//  Created by Dias Atudinov on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
